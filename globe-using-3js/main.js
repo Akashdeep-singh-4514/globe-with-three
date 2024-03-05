5a662943a73caf9ff59229ffcc6cef4efa7ccd68
Akashdeep-singh-4514 <waliaakash95@gmail.com>
2024-03-05 16:37:31 +0530

    sharpen rendering

--- globe-using-3js/main.js
+++ globe-using-3js/main.js
@@ -4,14 +4,19 @@ import * as THREE from 'three';
 //basic objects
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 10
-const renderer = new THREE.WebGLRenderer();
+//setting position is imp
+camera.position.z = 15
+//                              smoothing image
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
+// making picture clean
+renderer.setPixelRatio(window.devicePixelRatio)
 document.body.appendChild(renderer.domElement);
 
 // creating sphere
 const geometry = new THREE.SphereGeometry(5, 50, 50);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// covering sphere with map
+const material = new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load("./partials/globe.jpg") });
 const sphere = new THREE.Mesh(geometry, material); scene.add(sphere)
 scene.add(sphere)
 
@@ -20,5 +25,6 @@ scene.add(sphere)
 const animate = () => {
   requestAnimationFrame(animate)
   renderer.render(scene, camera)
+  sphere.rotation.y += 0.01
 }
 animate()
