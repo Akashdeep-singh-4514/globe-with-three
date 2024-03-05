7c270f1b3f3d45ed4c238ca58e779df5806825de
Akashdeep-singh-4514 <waliaakash95@gmail.com>
2024-03-05 16:19:47 +0530

    created spere

--- globe-using-3js/main.js
+++ globe-using-3js/main.js
@@ -1,12 +1,22 @@
 import * as THREE from 'three';
 
+
+//basic objects
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
+camera.position.z = 10
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+// creating sphere
+const geometry = new THREE.SphereGeometry(5, 50, 50);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const sphere = new THREE.Mesh(geometry, material); scene.add(sphere)
+scene.add(sphere)
+
+
+// animate function
 const animate = () => {
   requestAnimationFrame(animate)
   renderer.render(scene, camera)
