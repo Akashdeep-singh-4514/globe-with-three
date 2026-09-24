568f2de67a2cc314572bffd7e1158912502d5556
Akashdeep Singh <waliaakash95@gmail.com>
2026-09-24 18:13:10 +0530

    remove comments

--- globe-using-3js/main.js
+++ globe-using-3js/main.js
@@ -5,14 +5,12 @@ import atmosphereVertexShader from "./shaders/atmosphereVertex.glsl"
 import atmosphereFragmentShader from "./shaders/atmosphereFragments.glsl"
 import gsap from 'gsap';
 
-//basic objects
 const scene = new THREE.Scene();
 const canvasContainer = document.querySelector("#canvasContainer")
-// console.log(canvasContainer);
+
 const camera = new THREE.PerspectiveCamera(75, canvasContainer.offsetWidth / canvasContainer.offsetHeight, 0.1, 1000);
-//setting position is imp
+
 camera.position.z = 15
-//                              smoothing image
 const renderer = new THREE.WebGLRenderer({
   antialias: true,
   canvas: document.querySelector("canvas")
@@ -20,21 +18,13 @@ const renderer = new THREE.WebGLRenderer({
 
 
 renderer.setSize(canvasContainer.offsetWidth, canvasContainer.offsetHeight);
-// making picture clean
+
 renderer.setPixelRatio(window.devicePixelRatio)
 
 
-// creating sphere
+
 const geometry = new THREE.SphereGeometry(5, 50, 50);
-// covering sphere with map
 
-
-// const material = new THREE.MeshBasicMaterial({
-//   //  map: new THREE.TextureLoader().load("./partials/globe.jpg") 
-
-// });
-
-// creating custom shader
 const material = new THREE.ShaderMaterial({
   vertexShader,
   fragmentShader,
@@ -45,9 +35,7 @@ const material = new THREE.ShaderMaterial({
   }
 })
 const sphere = new THREE.Mesh(geometry, material); scene.add(sphere)
-// scene.add(sphere)
 
-//atmosphere mesh
 const atmosphereGeometry = new THREE.SphereGeometry(5, 50, 50)
 const atmosphereMaterial = new THREE.ShaderMaterial(
   {
